bb42ff076a22167d2b76ef882a580097a05d513a
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-02-13 12:13:12 +0100

    reported ticket page finished

--- public/tickets.js
+++ public/tickets.js
@@ -23,9 +23,11 @@ function getTable() {
   httpRequest1.onreadystatechange = function() {
     if (httpRequest1.readyState === 4 && httpRequest1.status === 200) {
       let response = JSON.parse(httpRequest1.response);
-      console.log(response.tickets);
       response.tickets.forEach(element => {
-        createTable(element.id, element.reporter, element.manufacturer, element.serial_number, element.description, element.reported_at, '<button>Delete</button>');
+        createTable(element.id, element.reporter, element.manufacturer, element.serial_number, element.description, element.reported_at, '<button class="button">Delete</button>');
+        let buttons = document.querySelectorAll('.button');
+        console.log(buttons);
+        [...buttons].forEach((element, index) => {element.addEventListener('click', () => {deleteTicket(index + 1)})});
       });
     };
   };
@@ -33,3 +35,16 @@ function getTable() {
 
 createTable('ID', 'Reporter', 'Manufacturer', 'Serial number', 'Description', 'Date', 'Actions');
 getTable();
+
+function deleteTicket(ticketId) {
+  let httpRequest2 = new XMLHttpRequest();
+  httpRequest2.open('DELETE', `http://localhost:8080/tickets/${ticketId}`);
+  httpRequest2.setRequestHeader('content-type', 'application/json');
+  httpRequest2.send(null);
+  httpRequest2.onreadystatechange = function() {
+    if (httpRequest2.readyState === 4 && httpRequest2.status === 200) {
+      let response = JSON.parse(httpRequest1.response);
+      console.log(reponse.status);
+    };
+  };
+};
